98da5c49fe46952b7fcfd3afb50b62c3759a0c3d
TheAizen <andypatrickpro@gmail.com>
2025-11-21 08:53:36 +0300

    architecture front et back

---
+++ frontend/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+        'title': String,
+        'todos': Array
+    }
+};
+</script>
+
+<template>
+    <h5> {{ title }}</h5>
+    <ul v-if="todos.length > 0">
+        <li v-for="todo in todos">
+        {{ todo }}
+        </li>
+    </ul>
+    <slot v-else="" name="no-todos">
+        il n'y pas de todo
+    </slot>
+</template>
